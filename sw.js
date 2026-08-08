e811e556165ff0f3c4b231bb604919d96809581b
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-09 00:04:22 +0200

    Passe le cache PWA en V19.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sproochentest-v19.0.0';
+const CACHE = 'sproochentest-v19.1.0';
 const ASSETS = ['./','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
