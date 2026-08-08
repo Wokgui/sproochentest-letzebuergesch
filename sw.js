972bf44c97110709820005fccee18c79e5b6b02d
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-08 23:53:19 +0200

    V19: renouveler le cache PWA

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = 'sproochentest-v18.0.5';
+const CACHE = 'sproochentest-v19.0.0';
 const ASSETS = ['./','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET' || new URL(e.request.url).origin !== self.location.origin) return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(response => {
+  e.respondWith(fetch(e.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(e.request, copy));
     return response;
-  }).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : Response.error())));
+  }).catch(() => caches.match(e.request).then(r => r || (e.request.mode === 'navigate' ? caches.match('./index.html') : Response.error()))));
 });
